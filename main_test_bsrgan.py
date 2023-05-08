3e79cc9217685359e1a6de9355006df8e88b5657
phonhay103 <phonhay103@gmail.com>
2023-05-08 10:15:38 +0700

    update

--- main_test_bsrgan.py
+++ main_test_bsrgan.py
@@ -4,8 +4,8 @@ import torch
 
 from utils import utils_logger
 from utils import utils_image as util
-# from utils import utils_model
 from models.network_rrdbnet import RRDBNet as net
+from tqdm import tqdm
 
 
 """
@@ -33,33 +33,25 @@ This work was previously submitted to CVPR2021.
 
 
 def main():
-
     utils_logger.logger_info('blind_sr_log', log_path='blind_sr_log.log')
     logger = logging.getLogger('blind_sr_log')
 
-#    print(torch.__version__)               # pytorch version
-#    print(torch.version.cuda)              # cuda version
-#    print(torch.backends.cudnn.version())  # cudnn version
-
-    testsets = 'testsets'       # fixed, set path of testsets
-    testset_Ls = ['RealSRSet']  # ['RealSRSet','DPED']
-
-    model_names = ['RRDB','ESRGAN','FSSR_DPED','FSSR_JPEG','RealSR_DPED','RealSR_JPEG']
-    model_names = ['BSRGAN']    # 'BSRGANx2' for scale factor 2
-
-
-
-    save_results = True
-    sf = 4
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    testsets = 'testsets'
+    testset_L = 'full_img'
+    input_path = os.path.join(testsets, testset_L)
+    image_paths = os.listdir(input_path)
+    model_names = ['ESRGAN']
+    device = torch.device('cuda')
 
     for model_name in model_names:
-        if model_name in ['BSRGANx2']:
+        if model_name == 'BSRGANx2':
             sf = 2
+        else:
+            sf = 4
         model_path = os.path.join('model_zoo', model_name+'.pth')          # set model path
         logger.info('{:>16s} : {:s}'.format('Model Name', model_name))
 
-        # torch.cuda.set_device(0)      # set GPU ID
+        torch.cuda.set_device(2)
         logger.info('{:>16s} : {:<d}'.format('GPU ID', torch.cuda.current_device()))
         torch.cuda.empty_cache()
 
@@ -67,13 +59,6 @@ def main():
         # define network and load model
         # --------------------------------
         model = net(in_nc=3, out_nc=3, nf=64, nb=23, gc=32, sf=sf)  # define network
-
-#            model_old = torch.load(model_path)
-#            state_dict = model.state_dict()
-#            for ((key, param),(key2, param2)) in zip(model_old.items(), state_dict.items()):
-#                state_dict[key2] = param
-#            model.load_state_dict(state_dict, strict=True)
-
         model.load_state_dict(torch.load(model_path), strict=True)
         model.eval()
         for k, v in model.named_parameters():
@@ -81,43 +66,33 @@ def main():
         model = model.to(device)
         torch.cuda.empty_cache()
 
-        for testset_L in testset_Ls:
+        output_path = os.path.join(testsets, testset_L+'_'+model_name)
+        util.mkdir(output_path)
 
-            L_path = os.path.join(testsets, testset_L)
-            #E_path = os.path.join(testsets, testset_L+'_'+model_name)
-            E_path = os.path.join(testsets, testset_L+'_results_x'+str(sf))
-            util.mkdir(E_path)
+        logger.info('{:>16s} : {:s}'.format('Input Path', input_path))
+        logger.info('{:>16s} : {:s}'.format('Output Path', output_path))
 
-            logger.info('{:>16s} : {:s}'.format('Input Path', L_path))
-            logger.info('{:>16s} : {:s}'.format('Output Path', E_path))
-            idx = 0
+        # for idx, filename in enumerate(tqdm(image_paths)):
+        for idx, filename in enumerate(image_paths):
+            # --------------------------------
+            # (1) img_L
+            # --------------------------------
+            logger.info('{:->4d} --> {:<s} --> x{:<d} --> {:<s}'.format(idx, model_name, sf, filename))
+            img = util.imread_uint(os.path.join(input_path, filename), n_channels=3)
+            img = util.uint2tensor4(img)
+            img = img.to(device)
 
-            for img in util.get_image_paths(L_path):
+            # --------------------------------
+            # (2) inference
+            # --------------------------------
+            img = model(img)
 
-                # --------------------------------
-                # (1) img_L
-                # --------------------------------
-                idx += 1
-                img_name, ext = os.path.splitext(os.path.basename(img))
-                logger.info('{:->4d} --> {:<s} --> x{:<d}--> {:<s}'.format(idx, model_name, sf, img_name+ext))
-
-                img_L = util.imread_uint(img, n_channels=3)
-                img_L = util.uint2tensor4(img_L)
-                img_L = img_L.to(device)
-
-                # --------------------------------
-                # (2) inference
-                # --------------------------------
-                img_E = model(img_L)
-
-                # --------------------------------
-                # (3) img_E
-                # --------------------------------
-                img_E = util.tensor2uint(img_E)
-                if save_results:
-                    util.imsave(img_E, os.path.join(E_path, img_name+'_'+model_name+'.png'))
+            # --------------------------------
+            # (3) img
+            # --------------------------------
+            img = util.tensor2uint(img)
+            util.imsave(img, os.path.join(output_path, filename))
 
 
 if __name__ == '__main__':
-
     main()
